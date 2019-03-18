23fe002f80344820e380b1d50f358750d8f6420c
JeanKarin <jean.karin@gmail.com>
2019-03-18 13:03:44 +0100

    subida primeros archivos

---
+++ main.py
@@ -0,0 +1,61 @@
+#!C:\Python27
+"""
+Nombre: Juan A. Horrillo
+Fecha: 17/03/2019
+"""
+
+# Comprobamos datos del fichero
+def ComprobarFitxero(euro):
+	for i in range(len(euro)):
+			if (euro[i][7] not in semana or euro[i][9] not in meses):
+				print ("Error en los datos, revisar dia o mes.")
+				print ("Fallo en linea: " + str(i))
+				print ("Dia de la semana: " + euro[i][7] + "\nMes: " + euro[i][9])
+				break
+
+# Lectura fichero csv
+def LecturaFichero(Lectura):
+	numeros = []
+	fichero = open(Lectura,'r')
+	lineas = csv.reader(fichero)
+	for line in lineas:
+		numeros.append(line)
+
+	return numeros
+
+# Funcion principal
+def main():
+	# Definimos las variables
+	
+	# Tratamos los argumentos
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-f", "--file", help="Nombre de archivo a procesar")
+	args = parser.parse_args()
+
+	# Tratamos el archivo y escribimos en la base de datos
+	if args.file:
+		print ("Procesamos el archivo con nombre : ", args.file)
+		if (args.file == 'numeros.txt'):
+			euro = LecturaFichero(args.file)
+			ComprobarFitxero(euro)
+			consulta = SqlStatement(euro)
+			InsertData(consulta)
+		elif (args.file == 'millones.txt'):
+			euro = LecturaFichero(args.file)
+			consmill = SqlStatement2(euro)
+			InsertData(consmill)
+
+if __name__ == '__main__':
+	# Librerias
+	import argparse
+	import csv
+	from sql_insert import SqlStatement
+	from sql_insert import SqlStatement2
+	from mysql_con import *
+
+	# Variables globales
+	semana = ["'Martes'","'Viernes'"]
+	meses = ["'Enero'","'Febrero'","'Marzo'","'Abril'","'Mayo'","'Junio'","'Julio'","'Agosto'","'Septiembre'","'Octubre'","'Noviembre'","'Diciembre'"]
+	
+	# Ejecutamos funcion principal
+	main()
---
+++ sql_insert.py
@@ -0,0 +1,14 @@
+def SqlStatement(euro):
+	sql = []
+
+	for i in range(len(euro)):
+		sql.append("""INSERT INTO NUMEROS VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""" % ('NULL', euro[i][0], euro[i][1], euro[i][2], euro[i][3], euro[i][4], euro[i][5], euro[i][6], euro[i][7], euro[i][8], euro[i][9], euro[i][10]))
+	
+	return sql
+
+def SqlStatement2(euro):
+	sql = []
+
+	for i in range(len(euro)):
+		sql.append("""INSERT INTO MILLONES VALUES (%s,%s,%s)""" % ('NULL',euro[i][0],euro[i][1]))
+	return sql
